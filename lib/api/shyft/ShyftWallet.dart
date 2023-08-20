f8052838dd9b54d6bd6f295341b5654d8672bd4c
Lavensoft <46833976+lavensoft@users.noreply.github.com>
2023-08-21 01:25:12 +0700

    U Fake Data

--- lib/api/shyft/ShyftWallet.dart
+++ lib/api/shyft/ShyftWallet.dart
@@ -6,21 +6,41 @@ import 'package:ville/config/Config.dart';
 
 class ShyftWallet {
   Future<Map> getAllTokens() async {
-    final Map<String, dynamic> data = {
-      "network": Config.WALLET_NETWORK,
-      "wallet": Config.WALLET_PUBLIC
+    if(Config.SHYFT_API_ENABLED) {
+      final Map<String, dynamic> data = {
+        "network": Config.WALLET_NETWORK,
+        "wallet": Config.WALLET_PUBLIC
+      };
+
+      final http.Response response = await http.get(
+        Uri.https("api.shyft.to", "sol/v1/wallet/all_tokens", data),
+        headers: {
+          'Content-Type': 'application/x-www-form-urlencoded',
+          "x-api-key": Config.SHYFT_KEY
+        },
+      );
+
+      var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
+
+      return decodedResponse;
+    }
+
+    return {
+      "success": true,
+      "message": "1 tokens fetched successfully",
+      "result": [
+        {
+          "address": "HTrU8JQTLwaiHddqXgrX8dn7EAzLWjhDpkt2vk6VMTec",
+          "balance": 10000,
+          "associated_account": "EVcMm8XiN4Kkveenr2tCuAAGccv73QUZ36ffM56QSaGf",
+          "info": {
+            "name": "Ville Wheat Seed",
+            "symbol": "VLF",
+            "image": "https://nftstorage.link/ipfs/bafkreihf2jwoiubgvqlvhc4vjz5yajaitujvhls2n6zfkmmgc7tro2sfxu",
+            "decimals": 0
+          }
+        }
+      ]
     };
-
-    final http.Response response = await http.get(
-      Uri.https("api.shyft.to", "sol/v1/wallet/all_tokens", data),
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded',
-        "x-api-key": Config.SHYFT_KEY
-      },
-    );
-
-    var decodedResponse = jsonDecode(utf8.decode(response.bodyBytes)) as Map;
-
-    return decodedResponse;
   }
 }
--- lib/config/Config.dart
+++ lib/config/Config.dart
@@ -1,5 +1,6 @@
 class  Config {
   static const tileZoom = 3.0;
+  static const SHYFT_API_ENABLED = false;
   static const SHYFT_KEY = "FvtQ-DsN9H3YDIYw";
   static const WALLET_PUBLIC = "7snszC4Q7uCayNX7zZE9THHpeuRE4Mj6qSjmk3ct9MPu";
   static const WALLET_PRIVATE = "HUZFJZBJDvPYi7XwdRyc6j8528ZPXNpGHrdXuFW18cPkbxXwvCmpEj647ZFDBubaTt938iJNYmKU4spdMthiF3V";
